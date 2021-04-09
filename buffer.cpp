e82ed0caa54891c1a48b7937ada3eda5fd55c6c3
Hee Suk Yoon <tomyoon2@gmail.com>
2021-04-09 09:26:24 -0400

    Update buffer.cpp

--- buffer.cpp
+++ buffer.cpp
@@ -1,10 +1,13 @@
 #include <mutex>
 #include <thread>
+#include <fstream>
+#include <random>
 #include <vector>
+#include <alsa/asoundlib.h>
 #include "constants.h"
 #include "readMicrophone.cpp"
 #include "dsp.cpp"
-#include "writeSpeaker.cpp"
+#include "writeSpeaker2Channel2.cpp"
 
 using namespace std;
 fstream buffer_log = fstream(BUFFER_LOG_FILENAME, fstream::out);
@@ -17,42 +20,54 @@ fstream buffer_log = fstream(BUFFER_LOG_FILENAME, fstream::out);
  */
 int main(int argc, char *argv[])
 {
-    if (LOGS_ON)
-        buffer_log << getTimestamp() << ": Begin Log" << endl;
-
-    float inputBuffer[INPUT_BUFFER_SIZE];
-    int inputBufferIndex = 0;
-
-    float *outputBuffer;
-    outputBuffer = new float[OUTPUT_BUFFER_SIZE];
-    int outputBufferIndex = 0;
-
     mutex readMutex;
     mutex writeMutex;
+    bool input1_capture = false;
+    bool input1_ready = false;
+    bool output1_change = false;
+    bool output1_ready =true;
+    bool output2_ready = false;
     vector<thread> threads;
 
+    std::default_random_engine generator;
+    std::normal_distribution<float> distribution(0.0,1.0);
+    float* whiteNoise = (float*) malloc(sizeof(float)*BUFFER_SIZE);
+    for (int i=0; i< BUFFER_SIZE; ++i) {
+        whiteNoise[i] = distribution(generator);
+    }
+    
+    float max = 0;
+    for (int i = 0; i < BUFFER_SIZE; i ++)
+    {
+        if (max < abs(whiteNoise[i]))
+            max = abs(whiteNoise[i]);
+    }
+    for (int i =0 ; i<BUFFER_SIZE; i++)
+    {
+        whiteNoise[i] = 0.1*whiteNoise[i]/max;
+    }
+    
+    char* pcm_ds_input_buffer = (char*)malloc(BUFFER_SIZE * BITS_PER_SAMPLE / 8);
+    char* pcm_ds_output_buffer = (char*)malloc(BUFFER_SIZE * BITS_PER_SAMPLE / 8);
+    char* temp_output_buffer = (char*)malloc(BUFFER_SIZE * BITS_PER_SAMPLE / 8) ;
+       
+    for (int i = 0; i < BUFFER_SIZE*BITS_PER_SAMPLE / 8; i++)
+    {
+        pcm_ds_output_buffer[i] = 0;
+    }
+    
     // Thread to parse Microphone input
-    threads.push_back(thread(readMicrophone, inputBuffer, ref(inputBufferIndex), ref(readMutex)));
-    if (LOGS_ON)
-        buffer_log << getTimestamp() << ": Microphone thread added" << endl;
+    threads.push_back(thread(readMicrophone, pcm_ds_input_buffer, ref(readMutex), ref(input1_ready), ref(output1_ready), ref(input1_capture)));
 
     // Thread for Digital Signal Processing
-    threads.push_back(thread(dsp, inputBuffer, ref(inputBufferIndex), ref(readMutex), outputBuffer, ref(outputBufferIndex), ref(writeMutex)));
-    if (LOGS_ON)
-        buffer_log << getTimestamp() << ": DSP thread added" << endl;
+    threads.push_back(thread(dsp, pcm_ds_input_buffer, ref(readMutex), pcm_ds_output_buffer, ref(writeMutex), ref(input1_ready), ref(output1_ready), ref(output2_ready), ref(output1_change), temp_output_buffer, whiteNoise));
 
     // Thread to write to Speaker
-    threads.push_back(thread(writeSpeaker, outputBuffer, ref(outputBufferIndex), ref(writeMutex)));
-    if (LOGS_ON)
-        buffer_log << getTimestamp() << ": Speaker thread added" << endl;
+    //threads.push_back(thread(writeSpeaker, pcm_ds_output_buffer, ref(writeMutex), ref(input1_ready), ref(output1_ready), ref(input1_capture), ref(output2_ready), ref(readMutex), ref(output1_change), temp_output_buffer));
+    threads.push_back(thread(writeSpeaker, pcm_ds_output_buffer, ref(writeMutex), ref(input1_ready), ref(output1_ready), ref(input1_capture), ref(output2_ready), ref(readMutex) , temp_output_buffer ));
 
     for (int i = 0; i < threads.size(); i++)
     {
         threads[i].join();
     }
-    if (LOGS_ON)
-        buffer_log << getTimestamp() << ": Threads joined" << endl;
-
-    if (LOGS_ON)
-        buffer_log << getTimestamp() << ": End Log" << endl;
 }
